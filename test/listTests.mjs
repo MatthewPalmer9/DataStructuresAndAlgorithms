1ccf23624553513c95fdbfef3770009b11a03721
Matthew Palmer <matthewpalmer9@outlook.com>
2022-03-16 09:06:17 -0500

    Added tests for the .contains() method in the List class

--- test/listTests.mjs
+++ test/listTests.mjs
@@ -13,29 +13,29 @@ describe('List Class', () => {
         expect(list.listSize).to.equal(3);
     });
 
-    it('should successfully add an element with the append() method', () => {
+    it('should successfully add an element with the .append() method', () => {
         list.append("d");
         expect(list.dataStore).to.contain("d");
     });
     
-    it('should remove an element from the list using the remove() method', () => {
+    it('should remove an element from the list using the .remove() method', () => {
         list.append("d");
         expect(list.remove("d")).to.be.true;
         expect(list.dataStore).to.eql(["a", "b", "c"]);
     });
 
-    it('should transform an array to a list using the to_list() method', () => {
+    it('should transform an array to a list using the .to_list() method', () => {
         const list2 = new List().to_list([1,2,3]);
         expect(list2.dataStore).to.eql(([1,2,3]));
     });
 
-    it('should transform a list into an array using to_a() method', () => {
+    it('should transform a list into an array using .to_a() method', () => {
         const list3 = new List();
         expect(list3.to_list([]).to_a()).to.eql([]);
         expect(list3.to_list(["a", "b", "c"]).to_a()).to.eql(["a", "b", "c"])
     });
 
-    it('should return the length of the list using the length() method', () => {
+    it('should return the length of the list using the .length() method', () => {
         expect(list.length()).to.equal(3);
     });
 
@@ -46,5 +46,19 @@ describe('List Class', () => {
     it('should insert an element to a specific place in the list', () => {
         expect(list.insert("d", "b")).to.be.true;
         expect(list.dataStore).to.eql(["a", "b", "d", "c"]);
+    });
+
+    it('should clear the list when using the .clear() method', () => {
+        list.clear();
+        expect(list.dataStore).to.eql([]);
+        expect(list.listSize).to.equal(0);
+    });
+
+    it('should return true if .contains() method finds the element in the list', () => {
+        expect(list.contains("b")).to.be.true;
+    });
+    
+    it('should return false if .contains() method does not find the element in the list', () => {
+        expect(list.contains("x")).to.be.false;
     });
 });
