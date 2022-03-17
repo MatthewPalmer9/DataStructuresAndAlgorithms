2d756dccc46f756b51fef188d6d383e671f59186
Matthew Palmer <matthewpalmer9@outlook.com>
2022-03-17 14:55:08 -0500

    Transformed Linked List into a Doubly Linked List and added/revised tests.

--- linkedList.js
+++ linkedList.js
@@ -1,21 +1,25 @@
 function Node(element) {
     this.element = element;
     this.next = null;
+    this.previous = null;
 }
 
 module.exports = function LList() {
     this.head = new Node("head");
     this.find = find;
+    this.findLast = findLast;
+    // this.findPrevious = findPrevious;
     this.insert = insert;
     this.remove = remove;
     this.display = display;
-    this.findPrevious = findPrevious;
+    this.dispReverse = dispReverse;
 
     function insert(newElement, item) {
         // should insert a new element in front of an existing element in the linked list
         const newNode = new Node(newElement); // create new node with newElement
         const current = this.find(item); // find the node you want to link after
         newNode.next = current.next; // newNode.next should be assigned to current.next, whatever that may be
+        newNode.previous = current; // newNode.previous should be assigned to current
         current.next = newNode; // current.next should point to newly inserted node
     }
 
@@ -31,30 +35,57 @@ module.exports = function LList() {
         return str;
     }
 
-    function findPrevious(item) {
-        // should return the node behind the queried item
-        let currNode = this.head;
-        while(!(currNode.next == null) && (currNode.next.element != item)) { // while currNode is not null & currNode.next.element is not the 
-            currNode = currNode.next;                                        // item we are looking for.
-        }
-        return currNode;
-    }
+    // .findPrevious() no longer needed
+    // function findPrevious(item) {
+    //     // should return the node behind the queried item
+    //     let currNode = this.head;
+    //     while(!(currNode.next == null) && (currNode.next.element != item)) { // while currNode is not null & currNode.next.element is not the 
+    //         currNode = currNode.next;                                        // item we are looking for.
+    //     }
+    //     return currNode;
+    // }
 
     function remove(item) {
         // should remove a node from our list
-        let prevNode = this.findPrevious(item);
-        if(!(prevNode.next == null)) { // if the element behind this item points back to it (exists)...
-            prevNode.next = prevNode.next.next; // tell previous node to forget about me and point to the node after me
+        let currNode = this.find(item);
+        if(!(currNode.next == null)) { // if the node in front of this one points back to it (exists)...
+            currNode.previous.next = currNode.next; // point prev node to next node, forget this node
+            currNode.next.previous = currNode.previous; // point next node to prev node, forget this node
+            currNode.next = null;       // ...Make current node... //
+            currNode.previous = null; // ...forget other nodes... //
         };
+        
+        return (currNode.next == null && currNode.previous == null) ? true : false;
     };
 
+    function dispReverse() {
+        let currNode = this.head;
+        let str = "";
+        currNode = this.findLast();
+        while(!(currNode.previous == null)) {
+            str += currNode.element + " => "
+            currNode = currNode.previous;
+        }
+        str += this.head.element;
+        return str;
+    }
+
     /**
-     * HELPERS
+     * HELPERS / UTILs
      */
 
     function find(item) {
         let currNode = this.head;
         while(currNode.element != item) {
+            if(currNode.next == null) { return null };
+            currNode = currNode.next;
+        }
+        return currNode;
+    }
+
+    function findLast() { // finds the last node in our linked list
+        let currNode = this.head;
+        while(!(currNode.next == null)) {
             currNode = currNode.next;
         }
         return currNode;
--- test/linkedList.test.js
+++ test/linkedList.test.js
@@ -8,21 +8,36 @@ describe('LinkedList', () => {
         list.insert("James", "head");
         list.insert("Steven", "James");
     });
-
+    
     it('should insert a new element in front of an existing element in the linked list', () => {
-        expect(list.head.next).toEqual(list.find("James"));
+        list.insert("Tom", "Steven");
+        expect(list.head.next.next.next.element).toEqual("Tom");
     });
 
     it('should display all elements in the linked list', () => {
         expect(list.display()).toEqual("head => James => Steven");
     });
 
-    it('should return the node behind the queried item', () => {
-        expect(list.findPrevious("Steven")).toEqual(list.find("James"));
+    // .findPrevious() no longer needed in a doubly linked list
+    // it('should return the node behind the queried item', () => {
+    //     expect(list.findPrevious("Steven")).toEqual(list.find("James"));
+    // });
+
+    it('should display our list in reverse', () => {
+        expect(list.dispReverse()).toBe("Steven => James => head");
+    });
+
+    it('should find the node by element name', () => {
+        expect(list.find("Steven")).toBe(list.head.next.next);
+    });
+
+    it('should find the last node in our list', () => {
+        expect(list.findLast()).toBe(list.head.next.next);
     });
 
     it('should remove a node from the list', () => {
-        list.remove("Steven");
-        expect(list.find("James").next).toBe(null);
+        expect(list.remove("James")).toBe(true);
+        expect(list.find("James")).toBe(null);
+        expect(list.find("Steven").previous).toBe(list.head);
     });
 });
