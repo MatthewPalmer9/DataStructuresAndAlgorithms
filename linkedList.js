955c6c7f804359a8ddcdb2914d8c49d300f22e4b
Matthew Palmer <matthewpalmer9@outlook.com>
2022-03-17 12:54:09 -0500

    Added tests and implementation of Linked Lists.

--- linkedList.js
+++ linkedList.js
@@ -1,4 +1,4 @@
-function Node() {
+function Node(element) {
     this.element = element;
     this.next = null;
 }
@@ -13,14 +13,31 @@ module.exports = function LList() {
 
     function insert(newElement, item) {
         // should insert a new element in front of an existing element in the linked list
+        const newNode = new Node(newElement); // create new node with newElement
+        const current = this.find(item); // find the node you want to link after
+        newNode.next = current.next; // newNode.next should be assigned to current.next, whatever that may be
+        current.next = newNode; // current.next should point to newly inserted node
     }
 
     function display() {
         // should display all elements in the linked list
+        let str = "";
+        let currNode = this.head;
+        while(!(currNode.next == null)) { // while this node's .next() is not null
+            str += currNode.element + " => "
+            currNode = currNode.next;
+        }
+        str += currNode.element
+        return str;
     }
 
     function findPrevious(item) {
         // should return the node behind the queried item
+        let currNode = this.head;
+        while(!(currNode.next == null) && (currNode.next.element != item)) { // while currNode is not null & currNode element is not the 
+            currNode = currNode.next;                                           // item we are looking for.
+        }
+        return currNode;
     }
 
     function remove(item) {
--- test/linkedList.test.js
+++ test/linkedList.test.js
@@ -2,20 +2,22 @@ const LList = require('../linkedList.js');
 
 describe('LinkedList', () => {
 
-    let list = new LLink();
+    let list;
     beforeEach(() => {
-        
+        list = new LList();
+        list.insert("James", "head");
+        list.insert("Steven", "James");
     });
 
     it('should insert a new element in front of an existing element in the linked list', () => {
-        
+        expect(list.head.next).toEqual(list.find("James"));
     });
 
     it('should display all elements in the linked list', () => {
-
+        expect(list.display()).toEqual("head => James => Steven");
     });
 
     it('should return the node behind the queried item', () => {
-
+        expect(list.findPrevious("Steven")).toEqual(list.find("James"));
     });
 });
