5beb2d8cabee2d80100efe27f0802fcede9e96e7
Matthew Palmer <matthewpalmer9@outlook.com>
2022-03-18 12:10:46 -0500

    Wrote tests and implementation code for Hash Tables.

--- hashStructure.js
+++ hashStructure.js
@@ -3,17 +3,43 @@ module.exports = function HashTable() {
     this.hash = hash;
     this.showDistro = showDistro;
     this.put = put;
-    // this.get = get;
+    this.get = get;
 
     function hash(data) {
         // should return the hash value of passed in data
+        const H = 37;
+        let total = 0;
+        for(let i = 0; i < data.length; i++) {
+            total += H * total + data.charCodeAt(i);
+        }
+        total = total % this.table.length;
+        if(total < 0) {
+            total += this.table.length-1;
+        }
+        return parseInt(total);
     }
 
     function put(data) {
         // should successfully insert data to the Hash Table
+        let pos = this.hash(data);
+        this.table[pos] = data;
     }
 
     function showDistro() {
         // should return each table in the Hash Table
+        let n = 0;
+        let str = "";
+        for(let i = 0; i < this.table.sort().length; i++) {
+            if(this.table[i] != undefined && str.length === 0) {
+                str += this.table[i];
+            } else if(this.table[i] != undefined) {
+                str += ", " + this.table[i];
+            }
+        }
+        return str;
+    }
+
+    function get(key) {
+        return this.table[this.hash(key)];
     }
 }
--- test/hashStructure.test.js
+++ test/hashStructure.test.js
@@ -8,14 +8,25 @@ describe('Hash Table', () => {
     });
 
     it('should return the hash value of passed in data', () => {
-
+        expect(hashTable.hash("hello")).toEqual(20);
+        expect(hashTable.hash("Raymond")).toEqual(22);
+        expect(hashTable.hash("Clayton")).toEqual(58);
     });
 
     it('should successfully insert data to the Hash Table', () => {
-
+        hashTable.put("Raymond");
+        expect(hashTable.table[22]).toEqual("Raymond");
     });
 
     it('should return each table in the Hash Table', () => {
+        hashTable.put("Raymond");
+        expect(hashTable.showDistro()).toBe("Raymond");
+        hashTable.put("Clayton");
+        expect(hashTable.showDistro()).toBe("Clayton, Raymond");
+    });
 
+    it('should get the data from the Hash Table', () => {
+        hashTable.put("Raymond");
+        expect(hashTable.get("Raymond")).toEqual("Raymond");
     });
 });
